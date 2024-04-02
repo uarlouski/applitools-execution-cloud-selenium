a1c074c9235c0560df29608028ff82fd75f4dc42
Uladzislau Arlouski <uarlouski@gmail.com>
2024-04-02 12:11:59 +0300

    Configure for local visual check run

--- src/main/java/org/example/Run.java
+++ src/main/java/org/example/Run.java
@@ -8,36 +8,24 @@ import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.visualgrid.model.IosDeviceInfo;
 import com.applitools.eyes.visualgrid.model.IosDeviceName;
 import com.applitools.eyes.visualgrid.services.VisualGridRunner;
-import org.openqa.selenium.JavascriptExecutor;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Map;
 
-public class ExecutionCloudRun {
+public class Run {
     public static void main(String[] args) throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName("chrome");
-        String server = System.getenv("EYES_SERVER_URL");
-        caps.setCapability("applitools:eyesServerUrl", server);
-        String apiKey = System.getenv("API_KEY");
-        caps.setCapability("applitools:apiKey", apiKey);
-
-        WebDriver driver = new RemoteWebDriver(new URL(Eyes.getExecutionCloudURL()), caps);
-        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
         try {
-            executor.executeScript("applitools:startTest", Map.of(
-                    "testName", "test-name",
-                    "appName", "app-name"
-            ));
-
-            EyesRunner runner = new VisualGridRunner(5);
+            EyesRunner runner = new VisualGridRunner(1);
             Configuration configuration = new Configuration();
             configuration.addBrowser(new IosDeviceInfo(IosDeviceName.iPhone_14));
+
+            String server = System.getenv("EYES_SERVER_URL");
+            String apiKey = System.getenv("API_KEY");
             configuration.setServerUrl(server);
             configuration.setApiKey(apiKey);
             configuration.setAppName("My web app");
@@ -53,10 +41,6 @@ public class ExecutionCloudRun {
             eyes.check(Target.window());
 
             eyes.close(false);
-
-            executor.executeScript("applitools:endTest" , Map.of(
-                    "status", "Passed"
-            ));
         } finally {
             driver.quit();
         }
